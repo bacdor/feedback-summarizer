5987408d23ec7f7ba52b2b0eb602e6caf827f373
bacdor <bachlaj.dorian@gmail.com>
2024-10-10 13:06:39 -0500

    slight chat edit

--- utils/openai/chat.ts
+++ utils/openai/chat.ts
@@ -9,7 +9,14 @@ export async function analyzeKeyThemes(text: string) {
       {
         role: 'system',
         content:
-          'You are a feedback analyzer. You will be provided with users feedback about an app. Identify Key Themes. Focus on: 1.Positive Feedback: Look for recurring comments about what you’re doing well. This might include specific features, qualities, or processes that people appreciate. 2.Negative Feedback: Identify the common complaints or suggestions for improvement. These are often areas where focus on making changes is needed. 3.Suggestions: Note any recommendations or ideas for new features or approaches that the users propose.'
+          'You are a feedback analyzer. You will be provided with users feedback about an app. ' +
+          'Identify Key Themes. Focus on: ' +
+          '1.Positive Feedback: Look for recurring comments about what you are doing well. ' +
+          'This might include specific features, qualities, or processes that people appreciate. ' +
+          '2.Negative Feedback: Identify the common complaints or suggestions for improvement. ' +
+          'These are often areas where focus on making changes is needed. ' +
+          '3.Suggestions: Note any recommendations or ideas for new features or approaches ' +
+          'that the users propose.'
       },
       {
         role: 'user',
@@ -29,7 +36,15 @@ export async function categorizeFeedbackByType(text: string) {
       {
         role: 'system',
         content:
-          'You are a feedback analyzer. You will be provided with users feedback about an app. Categorize feedback by type and provide summarization for each based on the following tips along with some examples. Product/Feature Feedback: What do people think about the features and usability of your app? Are there any feature requests or issues that are repeatedly mentioned? User Experience (UX): How easy is it for users to navigate your app? Are they frustrated by any specific parts of the interface or workflow? Performance Issues: Are users mentioning performance problems like speed, bugs, or crashes? Support and Communication: How do users feel about the level of support they are receiving? Are they satisfied with how quickly their issues are addressed?'
+          'You are a feedback analyzer. You will be provided with users feedback about an app. ' +
+          'Categorize feedback by type and provide summarization for each based on the following tips along with some examples. ' +
+          'Product/Feature Feedback: What do people think about the features and usability of your app? ' +
+          'Are there any feature requests or issues that are repeatedly mentioned? ' +
+          'User Experience (UX): How easy is it for users to navigate your app? ' +
+          'Are they frustrated by any specific parts of the interface or workflow? ' +
+          'Performance Issues: Are users mentioning performance problems like speed, bugs, or crashes? ' +
+          'Support and Communication: How do users feel about the level of support they are receiving? ' +
+          'Are they satisfied with how quickly their issues are addressed?'
       },
       {
         role: 'user',
@@ -41,3 +56,35 @@ export async function categorizeFeedbackByType(text: string) {
 
   return response.choices[0].message.content;
 }
+
+// export async function assessFeedbackSource(
+//   text: string,
+//   userCount: number,
+//   nonUserCount: number
+// ) {
+//   const response = await openai.chat.completions.create({
+//     model: 'gpt-4o-mini',
+//     messages: [
+//       {
+//         role: 'system',
+//         content:
+//           'You are a feedback analyzer. ' +
+//           'You will be provided with users feedback about an app and user counts. ' +
+//           'Assess the feedback source. ' +
+//           'Who provided the feedback? Different sources of feedback may carry different weights. ' +
+//           'For example, feedback from paying customers may be more critical than from free users. ' +
+//           'Feedback from power users: Are certain individuals giving more insightful feedback because they use the app heavily? ' +
+//           'Consider segmenting feedback from these users for deeper analysis. ' +
+//           'Analyze the proportion of feedback from users vs non-users based on the provided counts. ' +
+//           'Determine if there are any significant differences in the feedback between these two groups.'
+//       },
+//       {
+//         role: 'user',
+//         content: `${text}\n\nUser Count: ${userCount}\nNon-User Count: ${nonUserCount}`
+//       }
+//     ],
+//     max_tokens: 800
+//   });
+
+//   return response.choices[0].message.content;
+// }
