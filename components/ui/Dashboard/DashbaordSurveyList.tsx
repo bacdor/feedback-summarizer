8568d603106757994cd223518674cd26b3cfd4d8
bacdor <bachlaj.dorian@gmail.com>
2024-10-10 20:22:40 -0500

    dashboard ui

--- components/ui/Dashboard/DashbaordSurveyList.tsx
+++ components/ui/Dashboard/DashbaordSurveyList.tsx
@@ -59,14 +59,16 @@ export default function DashboardSurveyList({
 
   return (
     <div className="min-h-[calc(100dvh-8rem)] p-2 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Surveys</h2>
+      <h2 className="text-2xl text-[#111] font-bold mb-4 text-center">
+        Survey List
+      </h2>
       <table className="min-w-full table-auto border-collapse">
         <thead>
-          <tr className="border-b bg-gray-100">
-            <th className="px-6 py-4 text-left font-medium">Survey</th>
-            <th className="px-6 py-4 text-left font-medium">Actions</th>
-            <th className="px-6 py-4 text-left font-medium">Edit</th>
-            <th className="px-6 py-4 text-left font-medium">Analyze</th>
+          <tr className="border-b bg-[var(--color-primary)]">
+            <th className="px-6 py-4 text-left font-medium w-1/2"></th>
+            <th className="px-6 py-4 text-center font-medium w-1/6">Analyze</th>
+            <th className="px-6 py-4 text-center font-medium w-1/6">Edit</th>
+            <th className="px-6 py-4 text-center font-medium w-1/6">Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -77,39 +79,76 @@ export default function DashboardSurveyList({
                   {/* Combined Name and Description */}
                   <td className="px-6 py-4">
                     <Link href={`/surveys/${survey.id}`} passHref>
-                      <h3 className="text-xl font-semibold text-indigo-600">
+                      <h3 className="text-xl font-semibold text-[#111]">
                         {survey.name}
                       </h3>
-                      <p className="text-gray-600">{survey.description}</p>
+                      <p className="text-[#666]">{survey.description}</p>
                     </Link>
                   </td>
 
-                  {/* Actions Column */}
-                  <td className="px-6 py-4">
-                    <button
-                      onClick={() => handleDeleteSurvey(survey.id)}
-                      className="text-red-500 hover:underline"
-                    >
-                      Delete
-                    </button>
-                  </td>
-
-                  {/* Edit Column */}
-                  <td className="px-6 py-4">
-                    <Link href={`/surveys/edit/${survey.id}`} passHref>
-                      <button className="text-blue-500 hover:underline">
-                        Edit
-                      </button>
-                    </Link>
-                  </td>
-
-                  {/* Analyze Column */}
-                  <td className="px-6 py-4">
+                  {/* Analyze Column - Centered */}
+                  <td className="px-6 py-4 text-center">
                     <Link href={`/surveys/analyze/${survey.id}`} passHref>
-                      <button className="text-green-500 hover:underline">
-                        Analyze
+                      <button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          id="search"
+                        >
+                          <path
+                            fill="var(--color-primary)"
+                            d="M15.6207067,15.6542822 C16.0072308,15.270377 16.6268293,15.270377 17.0133534,15.6542822 L17.0133534,15.6542822 L19.5680176,17.7164156 L19.6123694,17.7164156 C20.1292102,18.2388171 20.1292102,19.0857973 19.6123694,19.6081989 C19.0955285,20.1306004 18.2575639,20.1306004 17.7407231,19.6081989 L17.7407231,19.6081989 L15.6207067,17.1784678 L15.5402577,17.0876967 C15.390397,16.8980019 15.3076306,16.6615115 15.3076306,16.416375 C15.3076306,16.1303824 15.4202849,15.8561581 15.6207067,15.6542822 Z M8.57763961,-7.10542736e-15 C10.8525711,-7.10542736e-15 13.0343273,0.913436016 14.6429467,2.53936255 C16.2515662,4.16528909 17.1552792,6.37051871 17.1552792,8.66992606 C17.1552792,13.458194 13.3149392,17.3398521 8.57763961,17.3398521 C3.84034006,17.3398521 2.13162821e-14,13.458194 2.13162821e-14,8.66992606 C2.13162821e-14,3.88165812 3.84034006,-7.10542736e-15 8.57763961,-7.10542736e-15 Z"
+                            transform="translate(2 2)"
+                          ></path>
+                        </svg>
                       </button>
                     </Link>
+                  </td>
+
+                  {/* Edit Column - Centered */}
+                  <td className="px-6 py-4 text-center">
+                    <Link href={`/surveys/edit/${survey.id}`} passHref>
+                      <button>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          fill="none"
+                          id="edit"
+                        >
+                          <path
+                            fill="#111"
+                            d="M17.864 3.6a1 1 0 0 0-1.414 0l-1.414 1.415 4.242 4.242 1.414-1.414a1 1 0 0 0 0-1.414L17.864 3.6zm0 7.072-4.243-4.243-8.9 8.9a1 1 0 0 0-.292.706v2.829a1 1 0 0 0 1 1h2.828a1 1 0 0 0 .707-.293l8.9-8.9z"
+                          ></path>
+                        </svg>
+                      </button>
+                    </Link>
+                  </td>
+
+                  {/* Actions Column - Centered */}
+                  <td className="px-6 py-4 text-center">
+                    <button onClick={() => handleDeleteSurvey(survey.id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="var(--color-accent)"
+                        id="delete"
+                      >
+                        <path
+                          fill="var(--color-accent)"
+                          d="M15 3a1 1 0 0 1 1 1h2a1 1 0 1 1 0 2H6a1 1 0 0 1 0-2h2a1 1 0 0 1 1-1h6Z"
+                        ></path>
+                        <path
+                          fill="var(--color-accent)"
+                          fillRule="evenodd"
+                          d="M6 7h12v12a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V7Zm3.5 2a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 1 0v-9a.5.5 0 0 0-.5-.5Zm5 0a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 1 0v-9a.5.5 0 0 0-.5-.5Z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
                   </td>
                 </tr>
               )
