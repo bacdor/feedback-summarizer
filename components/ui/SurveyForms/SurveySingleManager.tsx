7cf2b7b7a88ee040f10d881777740ae244e996e1
bacdor <bachlaj.dorian@gmail.com>
2024-10-12 12:46:29 -0500

    updating title and desc fixed

--- components/ui/SurveyForms/SurveySingleManager.tsx
+++ components/ui/SurveyForms/SurveySingleManager.tsx
@@ -13,33 +13,26 @@ export default function SurveySingleManager({
   surveyTitle: initialTitle,
   surveyDescription: initialDescription
 }: Props) {
-  const [surveyTitle, setSurveyTitle] = useState(initialTitle || '');
-  const [surveyDescription, setSurveyDescription] = useState(
-    initialDescription || ''
-  );
+  const [title, setTitle] = useState(initialTitle || '');
+  const [description, setDescription] = useState(initialDescription || '');
 
-  const [stagedSurveyTitle, setStagedSurveyTitle] = useState(
-    initialTitle || ''
-  );
-  const [stagedSurveyDescription, setStagedSurveyDescription] = useState(
+  const [stagedTitle, setStagedTitle] = useState(initialTitle || '');
+  const [stagedDescription, setStagedDescription] = useState(
     initialDescription || ''
   );
 
   const handleBlur = async () => {
-    if (
-      surveyTitle !== stagedSurveyTitle ||
-      surveyDescription !== stagedSurveyDescription
-    ) {
+    if (title !== stagedTitle || description !== stagedDescription) {
       try {
         const response = await fetch('/api/surveys', {
-          method: 'POST',
+          method: 'PUT',
           headers: {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
             id: initialId,
-            surveyTitle,
-            surveyDescription
+            title,
+            description
           })
         });
 
@@ -49,8 +42,8 @@ export default function SurveySingleManager({
 
         console.log('Survey updated successfully');
         // Update staged values after successful update
-        setStagedSurveyTitle(surveyTitle);
-        setStagedSurveyDescription(surveyDescription);
+        setStagedTitle(title);
+        setStagedDescription(description);
       } catch (error) {
         console.error('Error updating survey:', error);
       }
@@ -62,15 +55,15 @@ export default function SurveySingleManager({
       <div className="py-4">
         <input
           className="text-3xl font-extrabold text-[var(--color-dark)] border-b-2 border-gray-300 focus:outline-none focus:border-[var(--color-primary)] rounded-lg px-4 py-2 shadow-sm hover:shadow-md w-full min-h-14 resize-y transition-shadow duration-200 ease-in-out"
-          value={surveyTitle}
-          onChange={(e) => setSurveyTitle(e.target.value)}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           onBlur={handleBlur}
           placeholder="Enter survey title"
         />
         <textarea
           className="mt-4 text-lg text-[var-(--color-dark)] italic border-b-2 border-gray-300 w-full focus:outline-none focus:border-[var(--color-primary)] rounded-lg px-4 py-2 shadow-sm hover:shadow-md w-full min-h-14 resize-y transition-shadow duration-200 ease-in-out"
-          value={surveyDescription}
-          onChange={(e) => setSurveyDescription(e.target.value)}
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
           onBlur={handleBlur}
           placeholder="Enter survey description"
         />
